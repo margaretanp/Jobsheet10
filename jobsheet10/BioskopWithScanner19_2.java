8ff6f98efc828bba7969584b533861bcd354a7ab
margaretanp <143424663+margaretanp@users.noreply.github.com>
2023-10-31 13:54:38 +0700

    Add files via upload

---
+++ jobsheet10/BioskopWithScanner19_2.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class BioskopWithScanner19_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[][] penonton = new String[4][2];
+        int baris;
+        int kolom;
+        String nama;
+        String next;
+
+        while (true) {
+            System.out.println("Menu:");
+            System.out.println("1. Input data penonton");
+            System.out.println("2. Tampilkan daftar penonton");
+            System.out.println("3. Exit");
+            System.out.print("Pilih menu (1/2/3): ");
+            int menu = sc.nextInt();
+            sc.nextLine();
+
+            switch (menu) {
+                case 1:
+                    while (true) {
+                        System.out.print("Masukkan nama: ");
+                        nama = sc.nextLine();
+                        System.out.print("Masukkan baris: ");
+                        baris = sc.nextInt();
+                        System.out.print("Masukkan kolom: ");
+                        kolom = sc.nextInt();
+                        sc.nextLine();
+
+                        penonton[baris - 1][kolom - 1] = nama;
+
+                        System.out.print("Input penonton lainnya? (y/n): ");
+                        next = sc.nextLine();
+
+                        if (next.equalsIgnoreCase("n")) {
+                            break;
+                        }
+                    }
+                    break;
+                case 2:
+                    System.out.println("Daftar Penonton:");
+                    for (int i = 0; i < 4; i++) {
+                        for (int j = 0; j < 2; j++) {
+                            System.out.print(penonton[i][j] + "\t");
+                        }
+                        System.out.println();
+                    }
+                    break;
+                case 3:
+                    System.out.println("Terima kasih!");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Pilihan menu tidak valid. Silakan pilih menu 1, 2, atau 3.");
+            }
+        }
+    }
+}
